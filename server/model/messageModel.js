18eb475223fee96c8696e89604d0cf66eaa95ed2
Michael Chatzidakis <xatzidak.m@gmail.com>
2022-10-01 15:54:07 +0300

    Added model routes and controller for messages

--- server/model/messageModel.js
+++ server/model/messageModel.js
@@ -3,10 +3,10 @@ const mongoose = require('mongoose');
 const messageSchema = new mongoose.Schema(
     {
         message: {
-        text: {
-            type: String,
-            required: true,
-        },
+            text: {
+                type: String,
+                required: true,
+            },
         },
         users: Array,
         sender: {
