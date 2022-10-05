c31454f1e0c09619df3742c2eb60b704f2e59cdf
Michael Chatzidakis <xatzidak.m@gmail.com>
2022-10-05 07:22:38 +0300

    changed logo

--- public/src/components/Contacts.jsx
+++ public/src/components/Contacts.jsx
@@ -1,6 +1,6 @@
 import React, {useState, useEffect} from 'react'
 import styled from 'styled-components'
-import Logo from '../assets/logo.svg'
+import Logo from '../assets/logojira.svg'
 
 function Contacts({contacts, currentUser, changeChat}) {
 
--- public/src/pages/Login.jsx
+++ public/src/pages/Login.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react'
 import styled from 'styled-components'
 import { Link, useNavigate } from 'react-router-dom'
-import Logo from '../assets/logo.svg'
+import Logo from '../assets/logojira.svg'
 import {ToastContainer, toast} from 'react-toastify'
 import 'react-toastify/dist/ReactToastify.css';
 import axios from 'axios'
